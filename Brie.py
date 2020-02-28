7936ba38a68730f05111f22f08856935c2dc56a4
Enda Mulville <endampmulville@gmail.com>
2020-02-28 19:39:38 +0000

    Work on Brie

--- Brie.py
+++ Brie.py
@@ -1,3 +1,8 @@
+import string
+import time
+
+
+
 class BrieNode:
     def __init__(self, letter, end, delimiter):
         self.letter = letter.upper()
@@ -7,7 +12,7 @@ class BrieNode:
 
     def add_child(self, letter, end = False, delimiter = False):
         letter = letter.upper()
-        child_node = BrieNode(letter, end)
+        child_node = BrieNode(letter, end, delimiter)
         self.children[letter] = child_node
         self.end = end
         self.delimiter = delimiter
@@ -21,17 +26,45 @@ class BrieNode:
     def get_end(self):
         return self.end
 
+    def get_delimiter(self):
+        return self.delimiter
+
+class BrieFirstNode:
+    def __init__(self, word, end, delimiter):
+        self.word = word.upper()
+        self.children = {}
+        self.end = end
+        self.delimiter = delimiter
+
+    def add_child(self, letter, end = False, delimiter = False):
+        letter = letter.upper()
+        child_node = BrieNode(letter, end, delimiter)
+        self.children[letter] = child_node
+        self.end = end
+        self.delimiter = delimiter
+
+    def get_letter(self):
+        return self.letter
+
+    def get_children(self):
+        return self.children
+
+    def get_end(self):
+        return self.end
+
+    def get_delimiter(self):
+        return self.delimiter
 
 class BrieStartNode:
     def __init__(self):
         self.children = {}
         self.end = False
+        self.delimiter = False
 
-    def add_child(self, letter, end = False):
-        letter = letter.upper()
-        child_node = BrieNode(letter, end)
-        self.children[letter] = child_node
-        pass
+    def add_child(self, word, end = False, delimiter = False):
+        word = word.upper()
+        child_node = BrieFirstNode(word, end, delimiter)
+        self.children[word] = child_node
 
     def get_children(self):
         return self.children
@@ -40,24 +73,50 @@ class BrieStartNode:
         return self.end
 
 
-def print_dict(node, word = ""):
+def print_dict_preffix(node, word = ""):
+    new_word = word
+
+    if node.get_delimiter() == True:
+        new_word = word + "#"
+
     if node.get_end() == True:
-        print(word)
+        print(new_word)
+        print("aaa")
 
     child_nodes = node.get_children()
+
     if not child_nodes:
         return
 
+    if node.get_delimiter() == True:
+        for child in child_nodes:
+            print_dict_suffix(child_nodes[child], new_word)
+    if node.get_delimiter() != True:
+        for child in child_nodes:
+            new_word = str(child) + word
+            print_dict_preffix(child_nodes[child], new_word)
+            #print_dict(child_node)
 
+def print_dict_suffix(node, word = ""):
+    if node.get_end() == True:
+        #print(word)
+        pass
+    child_nodes = node.get_children()
+
+    if not child_nodes:
+        return
+        
     for child in child_nodes:
         new_word = word + str(child)
-        print_dict(child_nodes[child], new_word)
+        print_dict_suffix(child_nodes[child], new_word)
         #print_dict(child_node)
 
 def main():
 
+    start_time = time.time()
+
     dictionary = open("dic.txt", "r")
-    clean_dictionary = []
+    clean_dictionary = list(string.ascii_uppercase)
 
     for word in dictionary:
         clean_dictionary.append(word[:-1])
@@ -66,26 +125,58 @@ def main():
 
     start = BrieStartNode()
 
-    for word in clean_dictionary:
-        children = [key for key in start.get_children()]
-        if word[0].upper() not in children:
-            start.add_child(word[0])
+    for word in clean_dictionary[:20]:
 
-        current_node = start.get_children()[word[0].upper()]
-        for letter in word[1:-1]:
-            letter = letter.upper()
-            child_nodes = current_node.get_children()
-            child_letters = [key for key in child_nodes]
+        start.add_child(word)
 
-            if letter.upper() not in child_letters:
-                current_node.add_child(letter)
+        current_node = start.get_children()[word]
 
-            current_node = child_nodes[letter]
+        for longer_word in clean_dictionary:
+            if word in longer_word:
+                current_node = start.get_children()[word]
+                prefix, _, suffix = longer_word.partition(word)
 
-        letter = word[-1].upper()
-        current_node.add_child(letter, end = True)
+                #TODO NEEDS CHANGING
 
-    print_dict(start)
+                #print(prefix, word, suffix)
+
+                for letter in prefix[::-1]:
+                    letter = letter.upper()
+                    child_nodes = current_node.get_children()
+                    child_letters = [key for key in child_nodes]
+
+                    if letter.upper() not in child_letters:
+                        current_node.add_child(letter)
+
+                    current_node = child_nodes[letter]
+
+                if suffix:
+                    current_node.add_child("#", delimiter = True)
+                    child_nodes = current_node.get_children()
+                    current_node = child_nodes["#"]
+
+                    for letter in suffix[:-1]:
+                        letter = letter.upper()
+                        child_nodes = current_node.get_children()
+                        child_letters = [key for key in child_nodes]
+
+                        if letter.upper() not in child_letters:
+                            current_node.add_child(letter)
+
+                        current_node = child_nodes[letter]
+
+                    letter = suffix[-1].upper()
+                    current_node.add_child(letter, end = True)
+
+                else:
+                    current_node.add_child("#", end = True, delimiter = True)
+
+    print("Done")
+    print("--- %s seconds ---" % (time.time() - start_time))
+    for child in start.get_children():
+        print(child)
+        print("~~~~~~~~~~~~~~~")
+        print_dict_preffix(start.get_children()[child])
 
 
 if __name__== "__main__":
